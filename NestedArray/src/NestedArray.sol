b8077c8d8c744c0020fdb49f3e88681b90f5479d
toludaree <isaactoluwani30@gmail.com>
2025-03-21 18:37:43 +0100

    solve NestedArray

--- NestedArray/src/NestedArray.sol
+++ NestedArray/src/NestedArray.sol
@@ -9,10 +9,19 @@ contract NestedArray {
     }
 
     /**
-     * This function is expected to get the sum of all members of each nested array and finally return the sum of all the nested sums
+     * This function is expected to get the sum of all members of each nested array
+     *      and finally return the sum of all the nested sums
      * Example: [[1,2], [3,4]] this should return 1 + 2 + 3 + 4 = 10
      */
     function getNestedSum() public view returns (uint256) {
-        // your code here
+        uint256 nestedSum = 0;
+
+        for (uint256 i = 0; i < arr.length; i++) {
+            for (uint256 j = 0; j < arr[i].length; j++) {
+                nestedSum += arr[i][j];
+            }
+        }
+
+        return nestedSum;
     }
 }
