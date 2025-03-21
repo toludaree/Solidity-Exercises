72a437a5db912b7e7deb8c5a5243b5984d2cf53a
toludaree <isaactoluwani30@gmail.com>
2025-03-21 19:02:11 +0100

    solve TicTacToe

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,46 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        uint256 check = 0;
+        
+        // left-to-right
+        for (uint256 i = 0; i < 3; i++) {
+            check = board[i][0] + board[i][1] + board[i][2];
+            if (check == 3) {
+                return true;
+            }
+            if (check == 0) {
+                return true;
+            }
+        }
+
+        // up-to-down
+        for (uint256 j = 0; j < 3; j++) {
+            check = board[0][j] + board[1][j] + board[2][j];
+            if (check == 3) {
+                return true;
+            }
+            if (check == 0) {
+                return true;
+            }
+        }
+
+        // across
+        check = board[0][0] + board[1][1] + board[2][2];
+        if (check == 3) {
+            return true;
+        }
+        if (check == 0) {
+            return true;
+        }
+        check = board[0][2] + board[1][1] + board[2][0];
+        if (check == 3) {
+            return true;
+        }
+        if (check == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
